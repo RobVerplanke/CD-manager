4360cd984b68b2533a2b652ddc94c32ca56b1506
Rob <rverplanke@gmail.com>
2024-04-07 00:01:21 +0200

    create controller-methods for adding albums, cd's and tracks

--- src/modules/Album.js
+++ src/modules/Album.js
@@ -1,10 +1,18 @@
 class Album {
-  constructor(title, artist, year) {
-    this.title = title; // Title of the album
-    this.artist = artist; // Artist who released the album
+  constructor() {
+    this.title = ''; // Title of the album
+    this.artist = ''; // Artist who released the album
     this.label = ''; // Album-label when applicable
-    this.year = year; // Release year
+    this.year = null; // Release year
     this.items = []; // CD's in the album
+  }
+
+  addItem(title, artist, label, year, items) {
+    this.title = title;
+    this.artist = artist;
+    this.label = label;
+    this.year = year;
+    this.items = items;
   }
 }
 
