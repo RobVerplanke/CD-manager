33c6011808b22409cfe13ad4c213333fd3f300c5
Rob <rverplanke@gmail.com>
2024-04-07 00:59:46 +0200

    Replace array with set objecttype to store items

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,25 +1,30 @@
 class Controller {
   constructor() {
 
-    this.albums = []; // Store album when applicable which can contain multiple cd's
-    this.cds = []; // Store each individual cd so they can be linked to albums as well as to tracks
-    this.tracks = []; // Store tracks individually because they can occur in multiple cd's or albums
+    this.albums = new Set(); // Store album when applicable which can contain multiple cd's
+    this.cds = new Set(); // Store each individual cd so they can be linked to albums as well as to tracks
+    this.tracks = new Set(); // Store tracks individually because they can occur in multiple cd's or albums
   }
 
   // Add a album to the list of owned albums
   addAlbum(album) {
-    this.albums.push(album);
+    this.albums.add(album);
   }
+
+  // removeItem(album){
+
+  // }
 
   // Add a cd to the list of owned cds
   addCD(cd) {
-    this.cds.push(cd);
+    this.cds.add(cd);
   }
 
   // Add a track to the list of owned tracks
   addTrack(track) {
-    this.tracks.push(track);
+    this.tracks.add(track);
   }
+
 }
 
 module.exports = Controller;
--- tests/Controller.test.js
+++ tests/Controller.test.js
@@ -12,7 +12,7 @@ describe('Controller', () => {
 
     it('should be able to add a album', () => {
         const newController = new Controller();
-
+        
         const album = { 
             title: 'New title',
             artist: 'New artist',
@@ -20,17 +20,14 @@ describe('Controller', () => {
             year: 1999,
             items: [{ cd: 'cd 1' }]  
         };
+        
+        const iterator = newController.albums.values();
 
         // Add dummy album to controllers albums storage
         newController.addAlbum(album);
 
-        expect(newController.albums.length).toBe(1); // Stored a album
-
-        expect(newController.albums[0].title).toBe('New title');
-        expect(newController.albums[0].artist).toBe('New artist');
-        expect(newController.albums[0].label).toBe('New label');
-        expect(newController.albums[0].year).toBe(1999);
-        expect(newController.albums[0].items.length).toBe(1);
+        const firstAlbum = iterator.next().value;
+        expect(firstAlbum).toEqual(album);
     });
 
     it('should be able to add a CD', () => {
@@ -45,17 +42,13 @@ describe('Controller', () => {
             items: [{ tracks: 'track 1' }]  
         };
 
-        // Add dummy cd to controllers albums storage
+        const iterator = newController.cds.values();
+
+        // Add dummy cd to controllers cds storage
         newController.addCD(cd);
 
-        expect(newController.cds.length).toBe(1); // Stored a cd
-
-        expect(newController.cds[0].title).toBe('New title');
-        expect(newController.cds[0].artist).toBe('New artist');
-        expect(newController.cds[0].album.length).toBe(1);
-        expect(newController.cds[0].label).toBe('New label');
-        expect(newController.cds[0].year).toBe(2002);
-        expect(newController.cds[0].items.length).toBe(1);
+        const firstCD = iterator.next().value;
+        expect(firstCD).toEqual(cd);
     });
 
     it('should be able to add a track', () => {
@@ -70,16 +63,30 @@ describe('Controller', () => {
             length: 5.3
         };
 
-        // Add dummy cd to controllers albums storage
+        const iterator = newController.tracks.values();
+
+        // Add dummy track to controllers tracks storage
         newController.addTrack(track);
 
-        expect(newController.tracks.length).toBe(1); // Stored a cd
-
-        expect(newController.tracks[0].title).toBe('New title');
-        expect(newController.tracks[0].artist).toBe('New artist');
-        expect(newController.tracks[0].cd.length).toBe(1);
-        expect(newController.tracks[0].label).toBe('New label');
-        expect(newController.tracks[0].year).toBe(2004);
-        expect(newController.tracks[0].length).toBe(5.3);
+        const firstTrack = iterator.next().value;
+        expect(firstTrack).toEqual(track);
     });
+
+    // it('should be able to remove a album', () => {
+    //     const newController = new Controller();
+
+    //     const album = { 
+    //         title: 'New title',
+    //         artist: 'New artist',
+    //         label: 'New label',
+    //         year: 1999,
+    //         items: [{ cd: 'cd 1' }]  
+    //     };
+
+    //     // Add dummy album to controllers albums storage
+    //     newController.addAlbum(album);
+
+    //     // Send message to remove the album
+    //     newController.removeItem(album);
+    // });
 });
